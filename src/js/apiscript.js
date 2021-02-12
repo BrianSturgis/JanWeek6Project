6b26fa06109f48e3394cd0c7479b0fe949e68db5
Brian Sturgis <sturujisu@gmail.com>
2021-02-12 15:04:52 -0800

    good pass for conversion fucntion

--- src/js/apiscript.js
+++ src/js/apiscript.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default class CurrencyConverter {
   static async getUSD(){
     try {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
+/* eslint-disable */
 import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConverter from './js/apiscript.js';
 
 
 
+function getRateJPY(response) {
+  let answer = parseFloat($("#userDollar").val());
+  if (response.conversion_rates){
+    $(".output").html(`The exchange rate from USD is ${response.conversion_rates.JPY * answer} YEN.`);
+  } else {
+    $(".outputError").html(`api error message: ${response['error-type']}`);
+    $(".output").html(`API response: ${response}`);
+  }
+}
 
+async function apiRateJPY() {
+  const response = await CurrencyConverter.getUSD();
+  getRateJPY(response);
+}
 
 $(document).ready(function () {
   $("#exchange").click(function () {
@@ -14,6 +27,9 @@ $(document).ready(function () {
     let userInput = parseFloat($("#userDollar").val());
     $(".output").html(userInput);
 
-    console.log(userInput);
+    $("#convert-button1").click(function () {
+      event.preventDefault();
+      apiRateJPY();
+    });
   });
 });
