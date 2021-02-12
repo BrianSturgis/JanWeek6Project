dc7c9d9d756f7497df798adf3934f3873c9b6eb6
Brian Sturgis <sturujisu@gmail.com>
2021-02-12 14:27:32 -0800

    good pass for user input

--- src/main.js
+++ src/main.js
@@ -4,3 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConverter from './js/apiscript.js';
 
+
+
+
+
+$(document).ready(function () {
+  $("#exchange").click(function () {
+    event.preventDefault();
+    let userInput = parseFloat($("#userDollar").val());
+    $(".output").html(userInput);
+
+    console.log(userInput);
+  });
+});
